4b2c9302aafba95174dd8dfecc4511bcb0066cb9
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-01 21:13:26 -0500

    Implement character management in DragonballComponent with dynamic addition and display of characters based on power levels

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,6 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   templateUrl: './dragonball.template.html',
 })
-export class DragonballComponent {}
+export class DragonballComponent {
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 10000,
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      power: 9000,
+    },
+    {
+      id: 3,
+      name: 'Gohan',
+      power: 8000,
+    },
+    {
+      id: 4,
+      name: 'Trunks',
+      power: 500,
+    },
+  ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    };
+  });
+  addCharacter(name: string, power: number) {
+    this.characters.update((current) => [...current, { id: current.length + 1, name, power }]);
+  }
+}
